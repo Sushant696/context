861f1829fa2cac4101fdf2e08cd8628ec4a00c08
sushant babu prasai <sushantprasaii7@gmail.com>
2023-11-12 23:18:32 +0545

    final commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -11,36 +11,35 @@ function Login() {
     e.preventDefault();
     setUser({ username, Password });
   };
-  
+
   return (
     <div className="flex flex-col items-center">
-    <div className="w-[500px]  border text-center">
-      <form onSubmit={handleSubmit}>
-
-      <h2>login</h2>
-      <input
-        className="border p-1 w-1/2 rounded-[8px] m-2"
-        type="text"
-        value={username}
-        placeholder="username"
-        onChange={(e) => setusername(e.target.value)}
-        />
-      <input
-        className="border w-1/2 p-1 rounded-[8px] m-2"
-        type="password"
-        placeholder="Password"
-        value={Password}
-        onChange={(e) => setPassword(e.target.value)}
-        />
-      <button
-        className="border py-2 px-4 text-[#FFF] bg-[#444] w-1/2 rounded-xl m-2"
-        type="submit"
-        >
-        submit
-      </button>
-          </form>
+      <div className="w-[500px]  border text-center">
+        <form onSubmit={handleSubmit}>
+          <h2>login</h2>
+          <input
+            className="border p-1 w-1/2 rounded-[8px] m-2"
+            type="text"
+            value={username}
+            placeholder="username"
+            onChange={(e) => setusername(e.target.value)}
+          />
+          <input
+            className="border w-1/2 p-1 rounded-[8px] m-2"
+            type="password"
+            placeholder="Password"
+            value={Password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="border py-2 px-4 text-[#FFF] bg-[#444] w-1/2 rounded-xl m-2"
+            type="submit"
+          >
+            submit
+          </button>
+        </form>
+      </div>
     </div>
-          </div>
   );
 }
 
